b96ab277618cde738d035660ef0551d48d1f3d78
Syeda-Kulsoom <skulsoom762@gmail.com>
2023-05-24 16:54:13 +0500

    Assignment#3

---
+++ app.js
@@ -0,0 +1,19 @@
+inp1 = document.getElementById('fName')
+inp2 = document.getElementById('surName')
+inp3 = document.getElementById('Email')
+inp4 = document.getElementById('passWord')
+inp5 = document.getElementById('dates')
+inp6 = document.getElementById('Months')
+inp7 = document.getElementById('Year')
+inp8 = document.getElementById('Gender')
+
+function register(){
+    console.log(inp1.value)
+    console.log(inp2.value)
+    console.log(inp3.value)
+    console.log(inp4.value)
+    console.log(inp5.value)
+    console.log(inp6.value)
+    console.log(inp7.value)
+    console.log(inp8.value)
+}
